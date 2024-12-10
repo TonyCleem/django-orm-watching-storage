201c37d0ad1151dc528b3b2c00ef433fd67af593
TonyCleem <tonycleem@gmail.com>
2024-12-10 22:34:32 +0700

    start learn django

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
 
+import django
+
+
+
 from django.core.management import execute_from_command_line
+from datacenter.models import Visit
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
+django.setup()
+
+if __name__ == '__main__':
+
+    
+    not_leaved_visits = Visit.objects.filter(leaved_at__isnull=True)
+    print(not_leaved_visits)
