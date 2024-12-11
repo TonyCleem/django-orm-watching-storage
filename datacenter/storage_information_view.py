1c51ee28027cfbe7637e84fe79878eeaaaf15055
TonyCleem <tonycleem@gmail.com>
2024-12-11 22:46:05 +0700

    starts execution step 12

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,7 +4,40 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+
+
+    #шаг 10
+    current_time =  localtime().replace(microsecond=0)
+    visits = Visit.objects.filter(leaved_at__isnull=True)
+
+    for visit in visits:
+        print(f'{employee.passcard} - Зашёл в хранилище, время по Москве:')
+        print(visit.entered_at)
+        print()
+        stay_time = current_time - visit.entered_at
+        print("Находится в хранилище:")
+        print(stay_time)
+
+    #шаг 11
+    employees_from_storage = Visit.objects.filter(leaved_at__isnull=True)
+    for employee in employees_from_storage:
+        employee_name = employee.passcard.owner_name
+        print(employee_name)
+
+
+
+
+    # Шаг 12
+    current_time =  localtime().replace(microsecond=0)
+    
+    employees_from_storage = Visit.objects.filter(leaved_at__isnull=True)
+
+    for employee in employees_from_storage:
+        employee_name = employee.passcard.owner_name
+        print(employee_name)
+
+
+
 
     non_closed_visits = [
         {
